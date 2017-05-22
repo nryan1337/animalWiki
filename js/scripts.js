f8784b4886f6bfd23a874e00bfc7a418bacca3b4
Nic Ryan <nryan@timelio.net.au>
2017-05-22 20:53:55 +1000

    animal wiki

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("#cat").click(function() {
+    $("#catInfo").toggle();
+  });
+
+  $("#yak").click(function() {
+    $("#yakInfo").toggle();
+  });
+
+  $("#bear").click(function() {
+    $("#bearInfo").toggle();
+  });
+
+});
