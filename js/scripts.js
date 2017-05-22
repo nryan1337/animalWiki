9f5d64acb233766eca9fb85b5c5e0b66407f6215
Nic Ryan <nryan@timelio.net.au>
2017-05-22 21:02:15 +1000

    hide other animal info on click

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
 
   $("#cat").click(function() {
     $("#catInfo").toggle();
+    $("#yakInfo").hide();
+    $("#bearInfo").hide();
   });
 
   $("#yak").click(function() {
     $("#yakInfo").toggle();
+    $("#catInfo").hide();
+    $("#bearInfo").hide();
   });
 
   $("#bear").click(function() {
     $("#bearInfo").toggle();
+    $("#yakInfo").hide();
+    $("#catInfo").hide();    
   });
 
 });
